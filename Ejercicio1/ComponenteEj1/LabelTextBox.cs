9b618c1ae80fe801768b57829bd1028a63e0f3fd
CojoCosta <dcosta1882000@gmail.com>
2025-12-03 09:20:33 +0100

    changes 1/12

---
+++ Ejercicio1/ComponenteEj1/LabelTextBox.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComponenteEj1
+{
+    public enum EPosicion
+    {
+        IZQUIERDA, DERECHA
+    }
+    public partial class LabelTextBox : UserControl
+    {
+        public LabelTextBox()
+        {
+            InitializeComponent();
+        }
+        private EPosicion posicion = EPosicion.IZQUIERDA;
+        [Category("Appearance")]
+        [Description("Indica si la Label se sitúa a la IZQUIERDA o DERECHA del Textbox")]
+        public EPosicion Posicion
+        {
+            set
+            {
+                if (Enum.IsDefined(typeof(EPosicion), value))
+                {
+                    posicion = value;
+                    recolocar();
+                }
+                else
+                {
+                    throw new InvalidEnumArgumentException();
+                }
+            }
+            get
+            {
+                return posicion;
+            }
+        }
+
+        //Pixeles de separación entre label y textbox
+        private int separacion = 0;
+        [Category("Mis Propiedades")] // O se puede meter en categoría Design.
+        [Description("Píxels de separación entre Label y Textbox")]
+        public int Separacion
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    separacion = value;
+                    recolocar();
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            get
+            {
+                return separacion;
+            }
+        }
+        void recolocar() { }
+    }
+}
---
+++ Ejercicio1/FormEjercicio1/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace FormEjercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTextBox1 = new ComponenteEj1.LabelTextBox();
+            this.SuspendLayout();
+            // 
+            // labelTextBox1
+            // 
+            this.labelTextBox1.Location = new System.Drawing.Point(24, 12);
+            this.labelTextBox1.Name = "labelTextBox1";
+            this.labelTextBox1.Posicion = ComponenteEj1.EPosicion.IZQUIERDA;
+            this.labelTextBox1.Separacion = 0;
+            this.labelTextBox1.Size = new System.Drawing.Size(350, 139);
+            this.labelTextBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComponenteEj1.LabelTextBox labelTextBox1;
+    }
+}
+

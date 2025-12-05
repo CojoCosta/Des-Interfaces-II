f0bc4ddcef236be20fc8b8041b0ac78a810f6006
CojoCosta <dcosta1882000@gmail.com>
2025-12-05 10:43:11 +0100

    changes 4/12

---
+++ Ejercicio2/Ejercicio2/EtiquetaAviso.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio2
+{
+    public partial class EtiquetaAviso : Control
+    {
+        public EtiquetaAviso()
+        {
+            InitializeComponent();
+        }
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            Graphics g = pe.Graphics;
+            SolidBrush b = new SolidBrush(this.ForeColor);
+            g.DrawString(this.Text, this.Font, b, 0, 0);
+        }
+    }
+}
